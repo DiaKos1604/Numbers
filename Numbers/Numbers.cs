0fb46de5cdca35204f72c682974e321fcb66a7ab
Diana <piotr@DESKTOP-87SFJ8Q>
2023-11-15 11:54:05 +0100

    Number game added

---
+++ Numbers/Numbers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podstawy
+{
+    public class Numbers
+    {
+        public void PlayGame()
+        {
+            Random random = new Random();
+            int numerPicks = random.Next(1, 101); 
+            int userPicks = 0;
+
+            Console.WriteLine("Zgadnij liczbę od 1 do 100");
+            while (userPicks != numerPicks)
+            {
+                string userInput = Console.ReadLine();
+                if (!int.TryParse(userInput, out userPicks))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wpisz liczbę.");
+                    Console.ResetColor();
+                    continue;
+                }
+
+                if (userPicks > numerPicks)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Moja liczba jest trochę mniejsza niż {userPicks} :)");
+                    Console.ResetColor();
+                }
+                else if (userPicks < numerPicks)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Moja liczba jest trochę większa niż {userPicks} :)");
+                    Console.ResetColor();
+                }
+            }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Zgadłeś/aś, moja liczba to: {numerPicks}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("     \\o/     ");
+            Console.WriteLine("      |      ");
+            Console.WriteLine("     / \\    ");
+            
+
+        }
+    }
+}
--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Podstawy;
+
+Numbers game = new Numbers();
+game.PlayGame();
